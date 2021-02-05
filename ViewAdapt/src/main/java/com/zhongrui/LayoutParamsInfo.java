3d6bb5d4a3d76ff3117e1650716590e3595462af
zhongrui <zhongrui@021.com>
2021-02-05 09:49:14 +0800

    修改属性名字

--- ViewAdapt/src/main/java/com/zhongrui/LayoutParamsInfo.java
+++ ViewAdapt/src/main/java/com/zhongrui/LayoutParamsInfo.java
@@ -17,33 +17,33 @@ public class LayoutParamsInfo {
 
     public void getAttributeSet(Context c, AttributeSet attrs) {
         TypedArray a = c.obtainStyledAttributes(attrs, R.styleable.LayoutAdapt_Layout);
-        widthAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_adapt_width, 0);
-        heightAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_adapt_height, 0);
+        widthAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_layout_adapt_width, 0);
+        heightAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_layout_adapt_height, 0);
 
-        int margin = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_adapt_margin, -1);
+        int margin = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_layout_adapt_margin, -1);
         if (margin >= 0) {
             leftMarginAdapt = margin;
             topMarginAdapt = margin;
             rightMarginAdapt = margin;
             bottomMarginAdapt = margin;
         } else {
-            int horizontalMargin = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_adapt_marginStart, -1);
-            int verticalMargin = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_adapt_marginEnd, -1);
+            int horizontalMargin = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_layout_adapt_marginStart, -1);
+            int verticalMargin = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_layout_adapt_marginEnd, -1);
             if (horizontalMargin >= 0) {
                 leftMarginAdapt = horizontalMargin;
                 rightMarginAdapt = horizontalMargin;
                 startMarginAdapt = horizontalMargin;
             } else {
-                leftMarginAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_adapt_marginLeft, 0);
-                rightMarginAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_adapt_marginRight, 0);
+                leftMarginAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_layout_adapt_marginLeft, 0);
+                rightMarginAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_layout_adapt_marginRight, 0);
             }
             if (verticalMargin >= 0) {
                 topMarginAdapt = verticalMargin;
                 bottomMarginAdapt = verticalMargin;
                 endMarginAdapt = verticalMargin;
             } else {
-                topMarginAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_adapt_marginTop, 0);
-                bottomMarginAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_adapt_marginBottom, 0);
+                topMarginAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_layout_adapt_marginTop, 0);
+                bottomMarginAdapt = a.getDimensionPixelSize(R.styleable.LayoutAdapt_Layout_layout_adapt_marginBottom, 0);
             }
         }
         a.recycle();
--- ViewAdapt/src/main/java/com/zhongrui/RelativeLayoutAdapt.java
+++ ViewAdapt/src/main/java/com/zhongrui/RelativeLayoutAdapt.java
@@ -55,7 +55,7 @@ public class RelativeLayoutAdapt extends RelativeLayout {
         if (getParent() == null) {
             return;
         }
-        /*如果XXXLayoutAdapt的父view是系统ViewGroup,并且自身有设置adapt_width或者adapt_height，则自己适配自己的宽高*/
+        /*如果XXXLayoutAdapt的父view是系统ViewGroup,并且自身有设置layout_adapt_width或者layout_adapt_height，则自己适配自己的宽高*/
         if (!(getParent() instanceof LayoutAdaptHelper.AdaptView)) {
             int selfWidth = mHelper.selfWidth;
             int selfHeight = mHelper.selfHeight;
